1e01599154ac70c78c1bcfb8dc6aad0a926a4672
Sergio Galdames <s.galdames.f@gmail.com>
2022-07-13 19:49:37 -0400

    Se actualizó el proyecto inicial. Listo para entrega de complementario 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,59 @@
+
+
 let distanciaKm= parseInt(prompt("Ingresa la distancia que andarás (en Kilometros*)"))
 let valorGas= parseFloat(prompt("Ingresa el valor de la nafta"))
-let rendimientoPorLitro= parseInt(prompt("Ingresa el rendimiento por Litros de tu vehiculo"))
+let rendimientoPorLitro= parseInt(prompt("Ingresa el rendimiento de Kilometros/ Litro de tu vehiculo"))
 let kilometrosRecorridos=""
+let moneda=prompt("Ingresa la opción de la moneda a evaluar.\nCLP) Pesos Chilenos\nARS) Pesos Argentinos\nMXN) Pesos Mexicanos\nUSD) Dolar\nEU) Euro\nBTC) Bit Coins")
 
 let consumo=distanciaKm/rendimientoPorLitro
 console.log("El consumo esperado para "+distanciaKm+"km es de: " +consumo+" lts")
 let gasto=valorGas*consumo
 console.log("El gasto esperado es de: " +gasto)
-let i=""
-for(let i=1; i<=distanciaKm; i ++);{
-    let resultado = consumo * i;
-    console.log("El gasto a los " +i+" km "+resultado )
+
+while(moneda!="CLP" && moneda!="ARS" && moneda!="MXN" && moneda!="USD" && moneda!="EU" && moneda!="BTC" ){
+    switch (moneda){
+        case "CLP":
+            alert("Listo!");
+            break;
+            case "ARS":
+            alert("Listo!");
+            break;
+            case "MXN":
+            alert("Listo!");
+            break;
+            case "USD":
+            alert("Listo!");
+            break;
+            case "EU":
+            alert("Listo!");
+            break;
+            case "BTC":
+            alert("Listo!");
+            break;
+            default:
+                alert("No ingresaste una moneda valida")
+                break;
+    }
+moneda=prompt("Ingresa otra opción de moneda:\nCLP) Pesos Chilenos\nARS) Pesos Argentinos\nMXN) Pesos Mexicanos\nUSD) Dolar\nEU) Euro\nBTC) Bit Coins")
 }
+
+let i=1
+for(let i=1; i<=distanciaKm; i ++){
+    let resultado = (consumo * i) / distanciaKm;
+    console.log("El gasto a los " +i+" km es "+resultado )
+}
+
+document.write("<p>Considerando que la distancia que vas a recorrer es de " +distanciaKm+"\n y que tu vehiculo rinde "+rendimientoPorLitro+" Kms/Lts:</p>")
+document.write("<p>El consumo esperado para "+distanciaKm+"km es de: " +consumo+" lts</p>")
+document.write("<p>El gasto esperado es de: " +gasto+" "+moneda+".</p>")
+
+/*
+let entrada = prompt("Ingresar un dato");
+//Repetimos con While hasta que el usuario ingresa "ESC"
+while(entrada != "ESC" ){
+    alert("El usuario ingresó "+ entrada);
+    //Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
+    entrada = prompt("Ingresar otro dato");
+}
+*/
